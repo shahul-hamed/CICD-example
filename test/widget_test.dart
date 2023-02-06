496c419355767d663c6eb3ae4a460ea6fc175b6a
shahulh <shahulh@entoo.com>
2023-02-06 09:48:36 +0530

    widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,6 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'dart:developer';
 import 'dart:io';
 
 import 'package:cicd_example/main.dart';
