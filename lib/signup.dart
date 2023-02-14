333dc8a48f9d19a3ebb91ca7e8b88740a9880658
shahul-hameed <shahul5.it@gmail.com>
2023-02-14 14:54:25 +0530

    sign up page added and service account file json used from system env

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,22 +1,22 @@
-import 'package:cicd_example/signup.dart';
+import 'package:cicd_example/login.dart';
 import 'package:flutter/material.dart';
 
 import 'home.dart';
 
-class Login extends StatefulWidget {
-  const Login({super.key});
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
 
   @override
-  LoginState createState() => LoginState();
+  SignUpState createState() =>SignUpState();
 }
 
-class LoginState extends State<Login> {
+class SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text("Login"),
+        title: const Text("Create Account"),
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -36,6 +36,17 @@ class LoginState extends State<Login> {
             ),
             const SizedBox(
               height: 10,
+            ),
+            const Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                key: Key("name"),
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Name',
+                    hintText: 'Enter valid user name '),
+              ),
             ),
             const Padding(
               //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
@@ -61,6 +72,19 @@ class LoginState extends State<Login> {
                     hintText: 'Enter password'),
               ),
             ),
+            const Padding(
+              padding:
+                  EdgeInsets.only(left: 20.0, right: 20.0, top: 15, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                key: Key("confirm_password"),
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Confirm Password',
+                    hintText: 'Enter password'),
+              ),
+            ),
             const SizedBox(
               height: 40,
             ),
@@ -70,7 +94,7 @@ class LoginState extends State<Login> {
               decoration: BoxDecoration(
                   color: Colors.blue, borderRadius: BorderRadius.circular(20)),
               child: ElevatedButton(
-                key: const Key('login'),
+                key: const Key('signup'),
                 onPressed: () {
                   Navigator.push(
                       context,
@@ -80,7 +104,7 @@ class LoginState extends State<Login> {
                               )));
                 },
                 child: const Text(
-                  'Login',
+                  'Sign up',
                   style: TextStyle(color: Colors.white, fontSize: 20),
                 ),
               ),
@@ -90,9 +114,9 @@ class LoginState extends State<Login> {
             ),
             InkWell(
                 onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>const SignUp()));
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>const Login()));
                 },
-                child: const Text('New User? Create Account'))
+                child: const Text('Already a User? Login here'))
           ],
         ),
       ),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,6 +8,7 @@
 import 'dart:io';
 
 import 'package:cicd_example/main.dart';
+import 'package:cicd_example/signup.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -31,6 +32,27 @@ void main() {
     // await tester.tap(find.byKey(const Key('login')));
     // await tester.pumpAndSettle(const Duration(seconds: 1));
     debugPrint("login test completed");
+  });
+  testWidgets('Sign up widgets smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MaterialApp(home: SignUp(),));
+
+    // Verify that our counter starts at 0.
+    expect(find.byKey(const Key("image")), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+    // find name,email and password fields
+    var name = find.byKey(const Key("name"));
+    expect(name, findsOneWidget);
+    var email = find.byKey(const Key("email"));
+    expect(email, findsOneWidget);
+    var pass = find.byKey(const Key("password"));
+    expect(pass, findsOneWidget);
+    await tester.enterText(email, "tester");
+    await tester.enterText(email, "test1@gmail.com");
+    await tester.enterText(pass, "123456");
+    // await tester.tap(find.byKey(const Key('login')));
+    // await tester.pumpAndSettle(const Duration(seconds: 1));
+    debugPrint("account creation test completed");
   });
 }
 class _MyHttpOverrides extends HttpOverrides {}
